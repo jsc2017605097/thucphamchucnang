7aa8cb0084166e5d501f32f90d392763072f14be
unknown <nguyendocuongbka@gmail.com>
2020-10-02 20:14:37 +0700

    Updates

---
+++ precache-manifest.a72b1bcd9eef6b4065e045aa5f66c5b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5613f46b565e063b3931913f12b8e5ac",
+    "url": "/thucphamchucnang/index.html"
+  },
+  {
+    "revision": "c858598401de60071d71",
+    "url": "/thucphamchucnang/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "773a727fb988cfe7bb58",
+    "url": "/thucphamchucnang/static/css/main.11327e48.chunk.css"
+  },
+  {
+    "revision": "c858598401de60071d71",
+    "url": "/thucphamchucnang/static/js/2.0a981a8a.chunk.js"
+  },
+  {
+    "revision": "14d3db2f0aae1bbe6b1122194ce299b3",
+    "url": "/thucphamchucnang/static/js/2.0a981a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773a727fb988cfe7bb58",
+    "url": "/thucphamchucnang/static/js/main.d9f67022.chunk.js"
+  },
+  {
+    "revision": "416f5ec4754481ba95c1",
+    "url": "/thucphamchucnang/static/js/runtime-main.213756c0.js"
+  },
+  {
+    "revision": "74aaeeb9757d6af381f302c7800647f5",
+    "url": "/thucphamchucnang/static/media/bg1.74aaeeb9.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/thucphamchucnang/static/media/logo.ee7cd8ed.svg"
+  }
+]);
